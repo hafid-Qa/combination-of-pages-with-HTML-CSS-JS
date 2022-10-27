0270537f04bab282f746aa595bb0c4c6648d299e
Hafid Qarchi <haqarchi@gmail.com>
2022-10-27 21:19:22 +0900

    added js fror changing color dynamically

--- javascript/index.js
+++ javascript/index.js
@@ -9,3 +9,36 @@ window.addEventListener("scroll", () => {
     navbarEl.classList.remove("active");
   }
 });
+
+// random color
+
+const containerEl = document.querySelector(".cards");
+
+for (let i = 0; i < 10; i++) {
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("card-random-color");
+  containerEl.appendChild(colorContainerEl);
+}
+
+const colorContainerEls = document.querySelectorAll(".card-random-color");
+
+const randomColor = () => {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let i = 0; i < colorCodeLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNumber, randomNumber + 1);
+  }
+  return colorCode;
+};
+
+const generateColor = () => {
+  colorContainerEls.forEach((element) => {
+    const newColorCode = randomColor();
+    element.style.backgroundColor = `#${newColorCode}`;
+    element.innerText = `#${newColorCode.toUpperCase()}`;
+  });
+};
+
+generateColor();
